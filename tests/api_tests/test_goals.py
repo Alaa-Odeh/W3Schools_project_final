52c804cc7a32ef09b8addf8f047d6ae58b3bcd92
Alaa-Odeh <Kharbosh.computer@gmail.com>
2024-03-19 13:14:02 +0200

    first push

--- tests/api_tests/test_goals.py
+++ tests/api_tests/test_goals.py
@@ -10,7 +10,7 @@ class TestGoals(unittest.TestCase):
         self.my_api = APIWrapper()
         self.url = self.my_api.url
         #self.token = self
-        self.token=os.getenv('MY_TOKEN')
+        self.token=os.getenv('SECRET_TOKEN')
         self.goals = Goals(self.my_api, self.url,self.token)
 ###
     def test(self):
